8e2f95724ac17b8abe76cf61a08449d4444bbd5c
batoul abou khlil <batoul.abou.khlil@student.howest.be>
2024-11-13 18:00:18 +0100

    Apply .gitignore to untracked files

---
+++ src/api/nventory-log/routes/nventory-log.ts
@@ -0,0 +1,7 @@
+/**
+ * nventory-log router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nventory-log.nventory-log');
---
+++ src/api/nventory-log/services/nventory-log.ts
@@ -0,0 +1,7 @@
+/**
+ * nventory-log service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::nventory-log.nventory-log');
