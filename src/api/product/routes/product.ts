e5959ae69d5424a0718b8ef64d4153f0fb14bddc
Stephen Bombeke <stephen.bombeke@student.howest.be>
2024-12-14 11:43:52 +0100

    products post setup

--- src/api/product/routes/product.ts
+++ src/api/product/routes/product.ts
@@ -3,6 +3,7 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { config } from 'process';
 
 export default factories.createCoreRouter('api::product.product');
 
@@ -21,6 +22,15 @@ module.exports = {
             method: 'GET',
             path: '/products/:id',
             handler: 'product.findOne',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+        {
+            method: 'POST',
+            path: '/products',
+            handler: 'product.create',
             config: {
                 policies: [],
                 middlewares: [],
